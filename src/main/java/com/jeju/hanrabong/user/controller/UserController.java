8bc4b5c15dba7f3bf16d4dfe75ab51d7ef1cfde6
HeungY <rkrkrkrk02@gmail.com>
2024-08-05 22:05:01 +0900

    feat: create ranking func

--- src/main/java/com/jeju/hanrabong/user/controller/UserController.java
+++ src/main/java/com/jeju/hanrabong/user/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.jeju.hanrabong.user.controller;
 
+import com.jeju.hanrabong.user.entity.User;
+import com.jeju.hanrabong.user.entity.UserDTO;
+import com.jeju.hanrabong.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/top10")
+    public ResponseEntity<List<User>> top10() {
+        return ResponseEntity.ok(userService.top10());
+    }
+
+    @PostMapping("/save")
+    public void saveUser(@RequestBody UserDTO userDTO) {
+        if (!userService.checkDup(userDTO.getNickname())) {
+            userService.saveUser(userDTO.getNickname(), userDTO.getScore());
+        }
+    }
 }
---
+++ src/main/java/com/jeju/hanrabong/user/entity/UserDTO.java
@@ -0,0 +1,11 @@
+package com.jeju.hanrabong.user.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String nickname;
+    private int score;
+}
